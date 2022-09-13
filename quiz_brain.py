7f339166fa21a51efebf8d472253cfd3c44d0c23
Eren Düzova <erenduzova@gmail.com>
2022-09-14 00:35:26 +0300

    Bug Fix
    Unescape html entities.

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,6 @@
+import html
+
+
 class QuizBrain:
 
     def __init__(self, q_list):
@@ -9,7 +12,8 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.questions_list[self.question_number]
         self.question_number += 1
-        return f"Q.{self.question_number}: {self.current_question.text}"
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
 
     def check_answer(self, u_answer):
         correct_answer = self.current_question.answer
