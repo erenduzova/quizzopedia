9b6aaeebe159f4b34bfef5d280b899a40ab48c1b
Eren Düzova <erenduzova@gmail.com>
2022-09-13 11:44:00 +0300

    Question Bank Created
    List of all questions.
    All questions changed with Question class versions and added to the question bank.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from data import question_data
 from ui import QuizzInterface
+from question_model import Question
+
+
+def create_question_bank(data):
+    tmp_bank = []
+    for question in data:
+        q_text = question["question"]
+        q_answer = question["correct_answer"]
+        new_question = Question(q_text, q_answer)
+        tmp_bank.append(new_question)
+    return tmp_bank
+
+
+question_bank = create_question_bank(data=question_data)
 
 quiz_ui = QuizzInterface()
-
-
